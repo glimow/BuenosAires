b0719fda709b7cf032e47960dfd87c818f740a5e
glimow <tristan.kalos@live.fr>
2017-03-31 10:06:50 +0200

    Update index.js
    Added comments

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var mongoose = require('mongoose');
 var subscriptions = require('../models/models.js');
 
 /* GET home page. */
+//here will be the site's main routes that returns HTML and not JSON
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'express' });
 });
 
 router.get('/subscriptions/csv',function(req, res, next) {
-
+//select only subscription that have been validated, and export them to CSV file for direct download
   subscriptions.find()
   .select({ "_id":0, "name": 1, "firstName": 1, "email": 1 }).where({"state":"validated"})
   .exec(function (err, subscriptions) {
@@ -21,6 +22,7 @@ router.get('/subscriptions/csv',function(req, res, next) {
 });
 
 router.get('/subscriptions/validation/:id',function(req, res, next) {
+  //Validation of a subscription by ID
   subscriptions.findOneAndUpdate({_id:req.params.id},{"state":"validated"}, function (err) {
     if (err) res.json(err);
     res.json({state:"success"})
